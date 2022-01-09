a0af48fb4728fa6c09dc40d5a689d974aa32ad6e
samdevegg <samdevegg55@gmail.com>
2022-01-09 13:32:24 +0530

    added list

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,10 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("In main of MyProj");
+        List<String> list = new ArrayList <String> ();
+        list.add("Apple");
+        list.add("Orange");
+        list.add("Banana");
+        System.out.println(list.size());
     }
 }
